da21caf6665761031660433faf1c395ddc431fdc
Irem Erturk <irem.erturk93@gmail.com>
2021-12-01 21:31:35 +0100

    [Day1] Part 2: refactor and update for working with window_size

--- src/01-01-12.py
+++ src/01-01-12.py
@@ -1,19 +1,28 @@
 import sys
 
-def count_depth_measurement_increases(input_file_path="../inputs/01-01-12.txt"):
+
+def count_depth_measurement_increases(input_file_path="../inputs/01-01-12.txt", window_size=1):
     prev = sys.maxsize # float('inf')
     count = 0
     with open(input_file_path) as file:
-        while (line := file.readline().strip()):
-            if line:
-                if int(line) > prev: count+=1
-            prev = int(line)
+        lines = [int(line.strip()) for line in file.readlines()]
+        i = 0
+        while(i <= len(lines)-window_size): # sliding window boundry
+            window_start = i
+            window_sum = 0
+            for w in range(window_size):
+                window_sum += lines[window_start + w]
+            if window_sum > prev: count +=1
+            prev = window_sum
+            i +=1
     return count
 
 
 if __name__ == '__main__':
 
-    number_of_increases = count_depth_measurement_increases()
-    print(number_of_increases) # Answer:1462
+    number_of_increases_w1 = count_depth_measurement_increases()
+    print(number_of_increases_w1) # Answer:1462
 
+    number_of_increases_w3 = count_depth_measurement_increases(window_size=3)
+    print(number_of_increases_w3) # Answer:1497
 
