a74e376d2bb614c1c625c6931eda0a4d1b117797
Irem Erturk <irem.erturk93@gmail.com>
2021-12-02 19:09:59 +0100

    [Day 2] Part 2 find the final position with aim

--- src/02-02-12.py
+++ src/02-02-12.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def find_position(input_file_path="../inputs/02-02-12.txt"):
+def find_position_v1(input_file_path="../inputs/02-02-12.txt"):
     horizontal_position, depth = 0, 0
     with open(input_file_path) as file:
         while (line := file.readline().strip()):
@@ -16,9 +16,29 @@ def find_position(input_file_path="../inputs/02-02-12.txt"):
     return horizontal_position, depth
 
 
+def find_position_v2(input_file_path="../inputs/02-02-12.txt"):
+    
+    horizontal_position, depth, aim = 0, 0, 0
+
+    with open(input_file_path) as file:
+        while (line := file.readline().strip()):
+            direction, unit = line.split(" ")
+            if direction == "forward":
+                horizontal_position += int(unit)
+                depth += (aim*int(unit))
+            else:
+                if direction == "up":
+                    aim -= int(unit)
+                else:
+                    aim += int(unit)
+    return horizontal_position, depth
+
 
 if __name__ == '__main__':
 
-    horizontal_position, depth = find_position()
+    horizontal_position, depth = find_position_v1()
     print(horizontal_position * depth) # 1561344
+
+    horizontal_position, depth = find_position_v2()
+    print(horizontal_position * depth) # 1848454425
 
